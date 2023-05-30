74d3d0f4e78681b17785537fd500e94fb7513db0
robinver <robin.vermote@telenet.be>
2023-05-30 19:22:50 +0900

    added navigation bar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">test</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="sign_up">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/sign_in'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
